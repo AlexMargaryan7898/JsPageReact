35fb875e101d0e690103daf2ecfdb08dd05cccf1
validName <aleks.margaryan.y@tumo.org>
2022-07-20 03:33:56 +0400

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import AppRouter from "./components/Router.jsx";
 
 const  App = () => {
- 
   return (
     <div className="App">
-    
         <AppRouter/>
-      
-   
       </div>
-  
   );
 }
-
 export default App;
